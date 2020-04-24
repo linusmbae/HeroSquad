d89e8491f2ef9808a00cb1885f0a652575a6a390
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-24 20:58:44 +0300

    create constructor

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -3,5 +3,19 @@ package models;
 import java.util.Objects;
 
 public class Hero {
+    private String mName;
+    private int mAge;
+    private String mSpecialPowers;
+    private String mWeakness;
+    private int mId;
+
+    public Hero(String mName, int mAge, String mSpecialPowers, String mWeakness, int mId) {
+        this.mName = mName;
+        this.mAge = mAge;
+        this.mSpecialPowers = mSpecialPowers;
+        this.mWeakness = mWeakness;
+        this.mId = mId;
+    }
+
 
 }
