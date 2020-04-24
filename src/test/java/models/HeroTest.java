a77b5a28dabd2132fbec5d636c2d64387d547247
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-24 23:00:01 +0300

    test hero instantiates with name

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,27 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Test
+public void heroIsCreatedCorrectly_true() throws Exception
+{
+    Hero hero=createNewHero();
+    assertEquals(true,hero instanceof Hero);
+}
 
+    @Test
+    public void heroInstantiatesWithName() throws Exception {
+        Hero hero=createNewHero();
+        assertEquals("Super Girl",hero.getmName());
+    }
+
+
+    public Hero createNewHero()
+    {
+        return new Hero("Super Girl",35,"Flying", "stones",1);
+
+    }
 }
