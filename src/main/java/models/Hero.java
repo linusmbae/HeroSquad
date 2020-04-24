b495022ac0bb34b8010760732a065974fa567367
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-24 21:02:05 +0300

    create method equals and hashCode

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -16,6 +16,23 @@ public class Hero {
         this.mWeakness = mWeakness;
         this.mId = mId;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return mAge == hero.mAge &&
+                mId == hero.mId &&
+                Objects.equals(mName, hero.mName) &&
+                Objects.equals(mSpecialPowers, hero.mSpecialPowers) &&
+                Objects.equals(mWeakness, hero.mWeakness);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mName, mAge, mSpecialPowers, mWeakness, mId);
+    }
+
 
 
 }
