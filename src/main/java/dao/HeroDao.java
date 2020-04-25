f78aa7d1337c3e42dbe7e0cd7554e6b43a1f90ac
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-25 08:14:36 +0300

    interface heroDao complete

--- src/main/java/dao/HeroDao.java
+++ src/main/java/dao/HeroDao.java
@@ -1,5 +1,23 @@
 package dao;
 
-public interface HeroDao {
+import models.Hero;
 
+import java.util.List;
+
+public interface HeroDao {
+//    get entire list
+List<Hero> getAll();
+
+// create
+    void add (Hero hero);
+
+//    Read
+    Hero findById(int id);
+
+//    Update
+    void update(int id, String name, int age,String specialPowers,String weakness);
+
+//    Delete
+    void deleteById(int id);
+    void clearAll();
 }
